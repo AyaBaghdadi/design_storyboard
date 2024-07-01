5382e8db6a42e6cb5e4901488abb7476f3f65441
Aya <ayabaghdadi83@yahoo.com>
2024-07-01 11:07:17 +0300

    ||

--- storyboard_case/Home/HomeCollections.swift
+++ storyboard_case/Home/HomeCollections.swift
@@ -1,8 +1,113 @@
 //
-//  HomeCollections.swift
+//  BoardDelegate.swift
 //  storyboard_case
 //
-//  Created by Aya Baghdadi on 6/29/24.
+//  Created by Aya Baghdadi on 6/9/24.
 //
 
-import Foundation
+import UIKit
+
+extension HomeVC :  UICollectionViewDataSource , UICollectionViewDelegate , UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        if collectionView == collectionViewCategories {
+            return 20
+        }else if collectionView == collectionViewCategoriesSecond {
+            return 20
+        }else if collectionView == collectionViewCategoriesThird {
+            return 20
+        }else{ //
+            
+            collectionViewRestHeight.constant = 5 * 270
+            return 5
+        }
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        if collectionView == collectionViewCategories {
+            
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+            
+            
+            return cell
+            
+        }else if collectionView == collectionViewCategoriesSecond {
+            
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+            
+            
+            return cell
+            
+        }else if collectionView == collectionViewCategoriesThird {
+            
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+            
+            
+            return cell
+            
+        }else{
+            
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+            
+            
+            return cell
+            
+        }
+        
+    }
+
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+                    
+        if collectionView == collectionViewCategories {
+            
+            let itemWidth = collectionView.bounds.width / 2.2
+            
+            let itemHeight = collectionView.bounds.height
+            
+            return CGSize(width: itemWidth, height: itemHeight)
+            
+        }else if collectionView == collectionViewCategoriesSecond {
+            
+            let itemWidth = collectionView.bounds.width / 2.2
+            
+            let itemHeight = collectionView.bounds.height
+            
+            return CGSize(width: itemWidth, height: itemHeight)
+            
+        }else if collectionView == collectionViewCategoriesThird {
+            
+            let itemWidth = collectionView.bounds.width / 3
+            
+            let itemHeight = collectionView.bounds.height
+            
+            return CGSize(width: itemWidth, height: itemHeight)
+            
+        }else{
+            
+            let itemWidth = collectionView.bounds.width
+                        
+            return CGSize(width: itemWidth, height: 270)
+            
+        }
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        
+        return 0
+        
+    }
+    
+    func AssignXIB() {
+                        
+        collectionViewCategories.register(UINib(nibName: "Category", bundle: nil), forCellWithReuseIdentifier: "cell")
+        collectionViewCategoriesSecond.register(UINib(nibName: "CategorySecond", bundle: nil), forCellWithReuseIdentifier: "cell")
+        collectionViewCategoriesThird.register(UINib(nibName: "CategoryThird", bundle: nil), forCellWithReuseIdentifier: "cell")
+        collectionViewRest.register(UINib(nibName: "Resturant", bundle: nil), forCellWithReuseIdentifier: "cell")
+
+    }
+}
--- storyboard_case/Home/HomeVC.swift
+++ storyboard_case/Home/HomeVC.swift
@@ -7,11 +7,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeVC: UIViewController {
 
+    @IBOutlet weak var collectionViewRestHeight: NSLayoutConstraint!
+    @IBOutlet weak var collectionViewRest: UICollectionView!
+    
+    @IBOutlet weak var collectionViewCategoriesThird: UICollectionView!
+    @IBOutlet weak var collectionViewCategoriesSecond: UICollectionView!
+    @IBOutlet weak var collectionViewCategories: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.AssignXIB()
+        
     }
 
 
